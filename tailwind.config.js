83c149726d911a4090741f02a1501885e1f6307b
Bha <bha@Jhsync254.afuya.co>
2025-07-17 12:00:26 +0300

    tailwind success

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./app/templates/**/*.html",  // Jinja2 templates
+    "./src/**/*.css"              // Tailwind input
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
